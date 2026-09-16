33ce49a2d143a0a6e9721452687fef041f47ff00
Ayushraj23-ay <Ayushraj23082006@gmail.com>
2026-09-17 02:19:35 +0530

    Solved: Next Greater Element II [Medium]

---
+++ LeetCode/Medium/Next Greater Element II/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+
+        for(int i = 0; i < n; i++) {
+
+            bool found = false;
+
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] > nums[i]) {
+                    ans[i] = nums[j];
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found == false) {
+                for(int j = 0; j < i; j++) {
+                    if(nums[j] > nums[i]) {
+                        ans[i] = nums[j];
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
