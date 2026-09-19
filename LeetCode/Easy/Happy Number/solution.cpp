5130c81d9aac07c918d8a94b1ae059b1436162ce
Ayushraj23-ay <Ayushraj23082006@gmail.com>
2026-09-19 16:41:42 +0530

    Solved: Happy Number [Easy]

---
+++ LeetCode/Easy/Happy Number/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+
+        unordered_set<int> visited;
+
+        while(n != 1 && visited.count(n) == 0) {
+
+            visited.insert(n);
+
+            int sum = 0;
+
+            while(n > 0) {
+
+                int digit = n % 10;
+
+                sum = sum + digit * digit;
+
+                n = n / 10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+};
